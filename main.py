59ec088b6798eaee528570495f662fddca231c25
ikura1 <ikura.lab@gmail.com>
2020-10-16 07:17:58 +0900

    refactore: import順変更

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
-from weather import Weather, WEATHER_EMOJI
 
 import emoji
 import tweepy
 
 from owm import OWM
+from weather import Weather, WEATHER_EMOJI
 
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
