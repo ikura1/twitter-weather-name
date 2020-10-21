22d3f2d4ede3cf31ddc1531b1b7de30966034402
ikura1 <ikura.lab@gmail.com>
2020-10-22 03:40:39 +0900

    feat: dbに登録された値を参照して名前を変更するようにした

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import os
 import emoji
 import googlemaps
 import tweepy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from weather_name.models import User
 from owm import OWM, Weather, WEATHER_EMOJI
+
 
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 WEATHER_TOKEN = os.getenv("WEATHER_TOKEN")
+
+engine = create_engine(os.getenv("DATABASE_URL"))
 
 
 def get_weather_emoji(text):
@@ -34,26 +38,53 @@ def get_location_point(address):
     return location["lat"], location["lng"]
 
 
-def run():
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-    user = api.me()
+def replace_weather_name(db_user, user):
     name = emoji.demojize(user.name)
     target = get_weather_emoji(name)
     if not target:
+        # 名前に絵文字がない場合
         return
-
-    point = get_location_point(user.location)
+    if not user.location:
+        # 地名が登録されていない場合
+        return
+    point = None
+    if db_user.location == user.location:
+        lat, lon = (db_user.latitude, db_user.longitude)
+        if lat and lon:
+            point = (lat, lon)
+    else:
+        point = get_location_point(user.location)
     if not point:
+        # 座標が取得できなかった場合
         return
 
+    if db_user.location != user.location:
+        db_user.location = user.location
+        db_user.latitude, db_user.longitude = point
     w = OWM(WEATHER_TOKEN)
     response = w.get_weather_by_location(*point)
     json = response.json()
     weather = Weather.from_dict(json)
     name = name.replace(target, weather.get_emoji())
-    api.update_profile(name=name)
+    return name
+
+
+def run():
+    Session = sessionmaker(engine)
+    session = Session()
+    users = session.query(User).all()
+    for db_user in users:
+        try:
+            auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+            auth.set_access_token(db_user.access_token, db_user.access_token_secret)
+            api = tweepy.API(auth)
+            twitter_user = api.me()
+            name = replace_weather_name(db_user, twitter_user)
+            if name:
+                api.update_profile(name=name)
+        except tweepy.error.TweepError:
+            session.delete(db_user)
+    session.commit()
 
 
 if __name__ == "__main__":
