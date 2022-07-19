83a13f2680dcd5c541bb6c9506a07c547ddf8a55
ikura1 <ikura.lab@gmail.com>
2022-07-19 23:33:17 +0900

    fix: postgres to postgresql

--- weather_name/settings.py
+++ weather_name/settings.py
@@ -13,5 +13,7 @@ env.read_env()
 
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
-SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL").replace(
+    "postgres://", "postgresql://"
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
